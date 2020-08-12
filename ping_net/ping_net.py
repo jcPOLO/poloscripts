08d4f8e6a0904d38d7ceac274a305d956e88e84a
jcpolo <jcpolo@ext.aragon.es>
2020-08-12 10:44:05 +0200

    added -f option to read networks from file

--- ping_net/ping_net.py
+++ ping_net/ping_net.py
@@ -1,7 +1,9 @@
 import getopt
 import sys
+import os
 import ipaddress
 import subprocess
+from typing import List, Dict, Set
 
 
 MAX_PROCESS = 10000
@@ -9,7 +11,7 @@ MAX_PROCESS = 10000
 
 def args():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hn:', ['help', 'net='])
+        opts, args = getopt.getopt(sys.argv[1:], 'hn:f:', ['help', 'net=', 'file='])
     except getopt.GetoptError:
         print(f'Usa -[n] [net]')
         sys.exit(2)
@@ -17,10 +19,14 @@ def args():
     if len(opts) != 0:
         for opt, arg in opts:
             if opt in ('-h', '--help'):
-                print(f'Usa -[f] [filename]')
+                print(f'Usa -[f|n] [filename|network] ')
                 sys.exit()
             elif opt in ('-n', "--net"):
-                return str(arg)
+                r = []
+                r.append(str(arg))
+                return r
+            elif opt in ('-f', '--file'):
+                return get_nets_from_file(str(arg))
             else:
                 print(f'Usa -[f] [filename]')
                 sys.exit(2)
@@ -28,56 +34,65 @@ def args():
         print(f'Usa -[f] [filename]')
 
 
-def main():
-    result = []
-    pings = {}
-    i = 0
+def get_nets_from_file(file: str) -> List[str]:
+    path =  os.getcwd()
+    filename = f'{path}/{file}'
+    with open(filename) as f:
+        nets = f.read()
+    r = nets.split()
+    return r
 
-    net = args()
+
+def main():
+
+    nets = args()
 
     try:
-        network = ipaddress.ip_network(net)
-        print(f'Working with {network}')
+        for net in nets:
+            result = []
+            pings = {}
+            i = 0
 
-        for host in network.hosts():
-            host = str(host)
+            network = ipaddress.ip_network(net)
+            print(f'Working with {network}')
 
-            pings[host] = subprocess.Popen(
-                ['ping', '-c2', '-n', '-i0.5', '-W1', host],
-                shell=False,
-                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-                )
-            i += 1
-            # print(i, end='_')
-            if i > MAX_PROCESS:
-                while pings:
-                    for host, ping in pings.items():
-                        # print(i, end='.')
-                        # check if the child process has ended
-                        if ping.poll() is not None:
-                            del pings[host]
-                            i -= 1
-                            if ping.returncode == 0:
-                                result.append(host)
-                            break
+            for host in network.hosts():
+                host = str(host)
 
-        while pings:
-            for host, ping in pings.items():
-                print('.', end='')
-                if ping.poll() is not None:
-                    del pings[host]
-                    if ping.returncode == 0:
-                        result.append(host)
-                    break
+                pings[host] = subprocess.Popen(
+                    ['ping', '-c2', '-n', '-i0.5', '-W1', host],
+                    shell=False,
+                    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+                    )
+                i += 1
+                if i > MAX_PROCESS:
+                    while pings:
+                        for host, ping in pings.items():
+                            # check if the child process has ended
+                            if ping.poll() is not None:
+                                del pings[host]
+                                i -= 1
+                                if ping.returncode == 0:
+                                    result.append(host)
+                                break
+
+            while pings:
+                for host, ping in pings.items():
+                    if ping.poll() is not None:
+                        del pings[host]
+                        if ping.returncode == 0:
+                            result.append(host)
+                        break
+
+            file = net.replace('/', '_') + '.txt'
+            with open(file, 'w+') as f:
+                for host_ip in result:
+                    f.write(host_ip + '\n')
 
     except TypeError:
         print("error")
         pass
 
-    file = net.replace('/', '_') + '.txt'
-    with open(file, 'w+') as f:
-        for host_ip in result:
-            f.write(host_ip + '\n')
 
 
 if __name__ == "__main__":
