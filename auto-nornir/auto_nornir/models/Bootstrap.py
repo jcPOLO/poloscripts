c91bbbfb5a6afaab25ecef8e234372e3d63ec826
BloodyMer <bloodymery@gmail.com>
2021-06-06 13:30:53 +0200

    remove logs from git. Bootstrap now creates the yaml file but the keys starts with underscore due to Device class private attributes... no idea how to solve this if I use this way. Need to change it.

--- auto-nornir/auto_nornir/models/Bootstrap.py
+++ auto-nornir/auto_nornir/models/Bootstrap.py
@@ -1,10 +1,15 @@
+import logging
 import configparser
 import yaml
+import pathlib
+from typing import Dict, Any
 from csv import DictReader
-from ..helpers import is_ip, check_directory, configure_logging
-from typing import Dict
+from auto_nornir.helpers import check_directory, configure_logging
+from auto_nornir.models.Device import Device
+from auto_nornir.exceptions import ValidationException
 
-import logging
+import pprint
+
 
 logger = logging.getLogger(__name__)
 
@@ -16,69 +21,57 @@ class Bootstrap(object):
     def __init__(
         self,
         ini_file: str = '../.global.ini',
-        csv_file: str = 'inventory.csv'
+        csv_file: str = 'inventory.csv',
+        encoding: str = "utf-8"
     ):
 
-        self.ini_file = ini_file
-        self.csv_file = csv_file
+        self.ini_file = pathlib.Path(ini_file).expanduser()
+        self.csv_file = pathlib.Path(csv_file).expanduser()
+        self.encoding = encoding
         self.load_inventory()
 
     def get_ini_vars(self) -> configparser:
-        try:
+        if self.ini_file.exists():
             config = configparser.RawConfigParser()
             config.read(self.ini_file)
             return config
-        except Exception as e:
-            raise e
 
     # Return a dictionary from imported csv file
     def import_inventory_file(self) -> dict:
         platform = 'ios'
-        try:
-            result = {}
+        result = {}
+        devices = {}
 
-            with open(self.csv_file, 'r') as csv_file:
-                csv_reader = DictReader(csv_file)
-                fields = 'hostname'
-                fields_set = set(fields.split(','))
-                csv_fields_set = set(csv_reader.fieldnames)
-                wrong_header_fields = list(fields_set - csv_fields_set)
-                if not wrong_header_fields:
-                    for row in csv_reader:
-                        hostname = row['hostname'] if is_ip(row['hostname']) else None
-
-                        # remove duplicated hostname
-                        if hostname not in result.keys():
-                            result[hostname] = {
-                                'hostname': hostname,
-                                'platform': platform,
-                                'groups': [
-                                    platform
-                                ]
-                            }
-                else:
-                    logger.info('{} not in csv header'.format(wrong_header_fields))
-                    exit()
-
+        with open(self.csv_file, 'r', encoding=self.encoding) as csv_file:
+            csv_reader = DictReader(csv_file)
+            fields = 'hostname'
+            csv_fields = set(csv_reader.fieldnames)
+            wrong_headers = False if fields in csv_fields else True
+            if not wrong_headers:
+                # create dict of Devices from CSV
+                for row in csv_reader:
+                    hostname = row['hostname']
+                    if hostname not in devices.keys():
+                        devices[hostname] = Device(**row)
+            else:
+                message = '{} not in csv header'.format(wrong_headers)
+                logger.error(message)
+                raise ValidationException("fail-config", message)
+            for h, n in devices.items():
+                result[h] = n.__dict__
+            pp = pprint.PrettyPrinter(indent=2)
+            pp.pprint(result)
             return result
-        except Exception as e:
-            logger.error('{} Error'.format(e))
-            raise e
 
     def load_inventory(self) -> None:
         self.create_hosts_yaml(self.import_inventory_file())
 
     @staticmethod
     def create_hosts_yaml(d: Dict) -> None:
-        try:
-            file = 'hosts.yaml'
-            path = './inventory/'
-            filename = f'{path}{file}'
-            yml = yaml.dump(d)
-
-            check_directory(path)
-
-            with open(filename, 'w') as f:
-                f.write(yml)
-        except Exception as e:
-            raise e
+        file = 'hosts.yaml'
+        path = './inventory/'
+        filename = f'{path}{file}'
+        yml = yaml.dump(d)
+        check_directory(path)
+        with open(filename, 'w') as f:
+            f.write(yml)
--- auto-nornir/auto_nornir/models/Device.py
+++ auto-nornir/auto_nornir/models/Device.py
@@ -39,24 +39,30 @@ class Device(object):
         if not a:
             raise ValidationException("hostname cannot be empty")
         if not is_ip(a):
-            raise ValidationException(
-                "fail-config",
-                "hostname '{}' must be a valid IPv4 address".format(a)
-            )
+            message = "hostname '{}' must be a valid IPv4 address".format(a)
+            raise ValidationException("fail-config", message)
         self._hostname = a
 
     @platform.setter
     def platform(self, a):
         platforms = ['ios', 'nxos']
         if a not in platforms:
-            raise ValidationException(
-                "fail-config",
-                "platform '{}' is not in a supported. Supported: {}".format(
-                    a,
-                    ', '.join(platforms)
-                )
-            )
+            platforms_str = ', '.join(platforms)
+            message = "platform '{}' is not in a supported. Supported: {}".format(a, platforms_str)
+            raise ValidationException("fail-config", message)
         self._platform = a
+
+    @property
+    def port(self):
+        return self._port
+
+    @port.setter
+    def port(self, a):
+        if 65535 > int(a) > 0:
+            self._port = int(a)
+        else:
+            message = "port '{}' is not a valid port number".format(a)
+            raise ValidationException("fail-config", message)
 
     def run(self):
         return self.__dict__
